1213e429c0c2d433d836c3755d893948a5d14e24
romanzc2011 <romanzc2011@gmail.com>
2025-10-20 19:31:28 -0500

    update

--- Src/main.c
+++ Src/main.c
@@ -11,6 +11,7 @@
 static void GPIOA_Init(void);
 static void GPIOD_Init(void);
 static void TIM4_Init(void);
+static void TIMx_Init(int PSC, int ARR);
 
 __IO uint8_t counter = 0;
 __IO uint8_t pressed = 0;
@@ -18,13 +19,14 @@ __IO uint8_t pressed = 0;
 int main(void)
 {
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIODEN;
-	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN | RCC_APB1ENR_TIM2EN;
 
 	GPIOA_Init();
 	GPIOD_Init();
 	TIM4_Init();
 
 	while(1) {
+		/******* BUTTON PUSH SECTION ***********************/
 		// Turn off LED15 if interrupt happened
 		if ((GPIOD->ODR & (1U << LED15)) != 0) {
 			GPIOD->ODR &= ~(1U << LED15);
@@ -36,6 +38,9 @@ int main(void)
 			pressed = 0;
 			while ((GPIOA->IDR & (1U << PA0)) != 0);
 		}
+
+		/******* BLINK LED14 via TIMER ***********************/
+
 	}
 }
 
@@ -43,7 +48,6 @@ int main(void)
 static void GPIOA_Init(void)
 {
 	GPIOA->MODER &= ~(3UL << (PA0 * 2));
-
 	GPIOA->PUPDR &= ~(3UL << (PA0 * 2));
 	GPIOA->PUPDR |=  (2UL << (PA0 * 2));
 }
@@ -54,22 +58,23 @@ static void GPIOD_Init(void)
     /* MODER: 01 = output for PD15 */
     GPIOD->MODER &= ~(3UL << (LED13 * 2));
     GPIOD->MODER |=  (1UL << (LED13 * 2));
-
-    /* Push-pull: clear bit 15 in OTYPER */
     GPIOD->OTYPER &= ~(1UL << LED13);
-
-    /* Low speed */
     GPIOD->OSPEEDR &= ~(3UL << (LED13 * 2));
-
-    /* No pull */
     GPIOD->PUPDR &= ~(3UL << (LED13 * 2));
 
-    // PD15  <-- add this block
+    // LED15
 	GPIOD->MODER   &= ~(3UL << (LED15 * 2));
 	GPIOD->MODER   |=  (1UL << (LED15 * 2));
 	GPIOD->OTYPER  &= ~(1UL << LED15);
 	GPIOD->OSPEEDR &= ~(3UL << (LED15 * 2));
 	GPIOD->PUPDR   &= ~(3UL << (LED15 * 2));
+
+	// LED12 --- This LED will blink 1/sec on TIM2
+	GPIOD->MODER   &= ~(3UL << (LED12 * 2));
+	GPIOD->MODER   |=  (1UL << (LED12 * 2));
+	GPIOD->OTYPER  &= ~(1UL << LED12);
+	GPIOD->OSPEEDR &= ~(3UL << (LED12 * 2));
+	GPIOD->PUPDR   &= ~(3UL << (LED12 * 2));
 }
 
 static void TIM4_Init(void)
@@ -89,6 +94,23 @@ static void TIM4_Init(void)
 	NVIC_SetPriority(TIM4_IRQn, 15);
 
 	TIM4->CR1 |= TIM_CR1_CEN;
+}
+
+static void TIMx_Init(TIM_TypeDef TIMx,  int PSC, int ARR)
+{
+	TIMx->CR &= ~TIM_CR1_CEN;
+	TIMx->ARR = ARR;
+	TIMx->PSC = PSC;
+
+	TIMx->CR1 &= ~TIM_CR1_DIR;
+	TIMx->CR1 &= ~TIM_CR1_OPM;
+
+	TIMx->DIER |= TIM_DIER_UIE;  // Enable Update interrupt to be generated
+	TIMx->EGR = TIM_EGR_UG;     // Event gen reg, UG bit
+	TIMx->SR = 0;               // Status reg, set 0 after interrupt event
+
+	NVIC_EnableIRQ()
+
 }
 
 /* TIM4 Interrupt handler */

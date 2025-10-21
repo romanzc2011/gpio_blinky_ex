dd95b6b0bd36a4cc3d9cd04c059d3ab273482251
romanzc2011 <romanzc2011@gmail.com>
2025-10-20 22:46:30 -0500

    update

--- Src/main.c
+++ Src/main.c
@@ -10,20 +10,23 @@
 
 static void GPIOA_Init(void);
 static void GPIOD_Init(void);
-static void TIM4_Init(void);
-static void TIMx_Init(int PSC, int ARR);
+static void TIMx_Init(TIM_TypeDef *TIMx, IRQn_Type irqn, uint16_t psc, uint32_t arr);
 
 __IO uint8_t counter = 0;
 __IO uint8_t pressed = 0;
 
 int main(void)
 {
+	uint16_t TIM4_PSC = 6;
+	uint32_t TIM4_ARR = (59999-1);
+
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIODEN;
 	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN | RCC_APB1ENR_TIM2EN;
 
 	GPIOA_Init();
 	GPIOD_Init();
-	TIM4_Init();
+
+	TIMx_Init(TIM4, TIM4_IRQn, TIM4_PSC, TIM4_ARR);  // Init TIM4
 
 	while(1) {
 		/******* BUTTON PUSH SECTION ***********************/
@@ -77,40 +80,23 @@ static void GPIOD_Init(void)
 	GPIOD->PUPDR   &= ~(3UL << (LED12 * 2));
 }
 
-static void TIM4_Init(void)
+static void TIMx_Init(TIM_TypeDef *TIMx, IRQn_Type irqn, uint16_t psc, uint32_t arr)
 {
-	TIM4->CR1 &= ~TIM_CR1_CEN;  // Disable timer clock                                                                                                             z
-	TIM4->ARR = 59999 - 1;
-	TIM4->PSC = 6;
+	TIMx->CR1 &= ~TIM_CR1_CEN;
 
-	TIM4->CR1 &= ~TIM_CR1_DIR;
-	TIM4->CR1 &= ~TIM_CR1_OPM;
+	TIMx->ARR = arr;
+	TIMx->PSC = psc;
 
-	TIM4->DIER |= TIM_DIER_UIE;
-	TIM4->EGR = TIM_EGR_UG;
-	TIM4->SR = 0;
+	TIMx->CR1 &= ~(TIM_CR1_DIR | TIM_CR1_OPM);  // upcount, continuous
 
-	NVIC_EnableIRQ(TIM4_IRQn);   // Enabling the TIM4 interrupt handler
-	NVIC_SetPriority(TIM4_IRQn, 15);
+	TIMx->EGR = TIM_EGR_UG;      // Load PSC/ARR now
+	TIMx->SR = 0;                // Clear flags
+	TIMx->DIER |= TIM_DIER_UIE;  // update interrupt
 
-	TIM4->CR1 |= TIM_CR1_CEN;
-}
+	NVIC_SetPriority(irqn, 15);
+	NVIC_EnableIRQ(irqn);
 
-static void TIMx_Init(TIM_TypeDef TIMx,  int PSC, int ARR)
-{
-	TIMx->CR &= ~TIM_CR1_CEN;
-	TIMx->ARR = ARR;
-	TIMx->PSC = PSC;
-
-	TIMx->CR1 &= ~TIM_CR1_DIR;
-	TIMx->CR1 &= ~TIM_CR1_OPM;
-
-	TIMx->DIER |= TIM_DIER_UIE;  // Enable Update interrupt to be generated
-	TIMx->EGR = TIM_EGR_UG;     // Event gen reg, UG bit
-	TIMx->SR = 0;               // Status reg, set 0 after interrupt event
-
-	NVIC_EnableIRQ()
-
+	TIMx->CR1 |= TIM_CR1_CEN;
 }
 
 /* TIM4 Interrupt handler */
